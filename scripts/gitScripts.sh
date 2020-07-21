7a69032615e71e0314e3e989b3d6cf49ddcb0dea
Satheesh C <satheesancchandran@gmail.com>
2020-07-21 17:45:44 +0530

    |#3|Rivu/Satheesh|Added hooks
    Added pre-commit pre-push commit-msg hooks which will check
    the commented text, lint errors, test and commit message

---
+++ scripts/gitScripts.sh
@@ -0,0 +1,69 @@
+#! /bin/bash
+
+cat <<EOF > .git/hooks/pre-commit
+
+npm test
+
+if [ \$? != 0 ]; then
+  echo "\n Tests are failing ...\n"
+  exit 1;
+fi;
+
+grep -w "//" *.js src/*.js test/*.js
+
+if [ \$? == 0 ]; then
+  echo "\nRemove the commented lines ...\n"
+  exit 1
+fi;
+
+npm run lint
+
+if [ \$? != 0 ]; then 
+  echo "\nLint errors ...\n"
+  exit 1;
+fi;
+
+EOF
+
+chmod +x .git/hooks/pre-commit;
+
+cat <<EOF > .git/hooks/pre-push
+
+npm test
+
+if [ \$? != 0 ];
+  then echo "\n Tests are failing ...\n";
+  exit 1;
+fi;
+
+grep -w "//" src/*.js test/*.js
+
+if [ \$? == 0];
+  then echo "\nRemove the commented lines ...\n"
+  exit 1;
+fi;
+
+npm run lint
+
+if [ \$? != 0 ]; then 
+  echo "\nLint errors ...\n"
+  exit 1;
+fi;
+
+
+EOF
+
+chmod +x .git/hooks/pre-push
+
+cat <<EOF > .git/hooks/commit-msg
+
+cat \$1 | head -1 | grep -E "^\|#[0-9]+\|[a-zA-Z/]+\|.+$"
+
+if [ \$? != 0 ]; then 
+  echo "\nCommit message error ... \n|#<Issue number>|<name1>/<name2>|<heading>\n"
+  exit 1;
+fi;
+
+EOF
+
+chmod +x .git/hooks/commit-msg
