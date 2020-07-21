c541469ad68a02eb3182d8606176ca9a53ef998b
Satheesh C <satheesancchandran@gmail.com>
2020-07-21 17:52:33 +0530

    |#3|Rivu/Satheesh|Fixed the lint error for the server

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
+const { log } = console;
 const express = require('express');
-const cookieSession = require('cookie-session');
-const morgan = require('morgan');
-const sqlite3 = require('sqlite3');
 
 const PORT = 8000;
 const app = express();
 
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
+  log(`${req.method} ${req.url}`);
   next();
 });
-app.listen(PORT, () => console.log(`listening on port ${PORT}...`));
+
+app.listen(PORT, () => log(`listening on port ${PORT}...`));
