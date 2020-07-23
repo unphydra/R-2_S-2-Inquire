f399b5982eaf938fe73b32b6fefa3b94f4c8319b
rivu <58026024+unphydra@users.noreply.github.com>
2020-07-23 12:13:39 +0530

    |#12|Rivu/Satheesh| deleted app.js

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const { static } = require('express');
 const app = express();
-const { serveHomepage } = require('./handlers');
+const { serveHomepage, reqLogin, handleLogin } = require('./handlers');
 
 app.use(morgan('tiny'));
 app.use(static('public', { index: '/html/home.html' }));
 
 app.get('/home', serveHomepage);
+app.get('/login', reqLogin);
+app.get('/user/auth', handleLogin);
 
 module.exports = { app };
