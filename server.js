32e1ad5321d357ad46fd555a4573ebd33c052a51
Satheesh C <satheesancchandran@gmail.com>
2020-07-23 17:29:49 +0530

    |#12|Rivu/Satheesh|Added sqlite date base
    Added the dataStore instance at the app.locals

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const sqlite = require('sqlite3').verbose();
 const { stdout } = require('process');
+const DataStore = require('./library/dataStore');
 const { app } = require('./src/router');
-const {env} = process;
-const PORT = env.PORT;
+const { env } = process;
+const { PORT, ClientID, ClientSecret, DatabaseUrl } = env;
+const db = new sqlite.Database(DatabaseUrl);
+const dataStore = new DataStore(db);
+
+app.locals = { ClientID, ClientSecret, dataStore };
 
 app.listen(PORT, () => stdout.write(`listening on port ${PORT}...\n`));
--- src/handlers.js
+++ src/handlers.js
@@ -1,22 +1,21 @@
 const request = require('superagent');
 const path = require('path');
-const { env } = process;
-const { ClientID, ClientSecret } = env;
 
 const reqLogin = function (req, res) {
+  const { ClientID } = req.app.locals;
   const redirectUri = 'http://localhost:8000/user/auth';
   const route = 'https://github.com/login/oauth/authorize';
   const url = `${route}?client_id=${ClientID}&redirect_uri=${redirectUri}`;
   res.redirect(url);
 };
 
-const getToken = function (code) {
+const getToken = function (code, ClientSecret, ClientID) {
   return request
     .post('https://github.com/login/oauth/access_token')
     .send({ code, ['client_secret']: ClientSecret, ['client_id']: ClientID })
     .set('Accept', 'application/json')
-    .then((res) => res.body)
-    .then((data) => data.access_token);
+    .then(res => res.body)
+    .then(data => data.access_token);
 };
 
 const getUserInfo = function (token) {
@@ -24,12 +23,13 @@ const getUserInfo = function (token) {
     .get('https://api.github.com/user')
     .set('User-Agent', 'r2s2-inquire')
     .set('Authorization', `token ${token}`)
-    .then((res) => res.body);
+    .then(res => res.body);
 };
 
 const handleLogin = async function (req, res) {
+  const { ClientID, ClientSecret } = req.app.locals;
   const code = req.query.code;
-  const token = await getToken(code);
+  const token = await getToken(code, ClientSecret, ClientID);
   const userInfo = await getUserInfo(token);
   res.send(userInfo);
 };
