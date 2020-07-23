19d3e7ecb3eac72fefd1b49b481e28d4d2bdac49
Satheesh C <satheesancchandran@gmail.com>
2020-07-23 19:54:17 +0530

    |#12|Rivu/Satheesh|Fixed the test failing for /home
    Fixed the bug for dotenv in test

---
+++ test/routerTest.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+require('dotenv').config({ path: './.env' });
+const { app } = require('../src/router');
+const statusCodes = { ok: 200 };
+
+describe('get', function () {
+  context('home', function () {
+    it('should give the home.html page ', function (done) {
+      request(app)
+        .get('/home')
+        .expect(statusCodes.ok)
+        .expect('Content-Type', 'text/html; charset=UTF-8', done)
+        .expect(/Your Questions/);
+    });
+  });
+});
