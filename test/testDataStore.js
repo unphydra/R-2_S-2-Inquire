bad5b5f21ecf5d870005658c56b2093ce6736711
rivu <58026024+unphydra@users.noreply.github.com>
2020-07-25 12:27:16 +0530

    |#13|Rivu/Satheesh| added dataSeeding to test DataStore

--- test/testDataStore.js
+++ test/testDataStore.js
@@ -1,5 +1,7 @@
 const { assert } = require('chai');
 const DataStore = require('../library/dataStore');
+const dataSeeding = require('./testDataSeeding');
+const { dropTables, createTables, insertIntoTables } = dataSeeding;
 const sqlite = require('sqlite3');
 const { env } = process;
 env.DatabaseUrl = './data/test.db';
@@ -9,6 +11,9 @@ describe('DataStore', function () {
   beforeEach(() => {
     const db = new sqlite.Database(env.DatabaseUrl);
     dataStore = new DataStore(db);
+    dropTables(db);
+    createTables(db);
+    insertIntoTables(db);
   });
 
   context('getAllQuestions', function () {
@@ -49,25 +54,52 @@ describe('DataStore', function () {
       ).catch(err => {
         assert.deepStrictEqual(err.message, message);
       });
-      assert.deepStrictEqual(actual, undefined);
+      assert.deepStrictEqual(actual, []);
     });
   });
 
   context('FindUser', function() {
-    it('should get user details when the user is in present', async () => {
-      const actual = await dataStore.findUser('123');
+    it('should get user details when the user is present', async () => {
+      const actual = await dataStore.findUser('58026024');
       assert.deepStrictEqual(
         actual, 
         {
-          id: 'u123',
-          name: 'test', 
-          username: 'test', 
-          email: 'test', 
-          bio: 'test', 
-          company: 'undefined', 
-          avatar: 'undefined'
-        }
+          id: 'u58026024', 
+          name: 'Rivu', 
+          username: 'unphydra',
+          company: 'thoughtworks',
+          email: 'rivu123@gmail.com',
+          avatar: 'https://avatars3.githubusercontent.com/u/58026024?v=4',
+          bio: 'hi i\'m a developer'
+        },
       );
+    });
+  });
+});
+
+describe('DataStore rejection', function () {
+  let dataStore;
+  beforeEach(() => {
+    const db = new sqlite.Database(env.DatabaseUrl);
+    dataStore = new DataStore(db);
+    dropTables(db);
+  });
+
+  context('getAllQuestions', async() => {
+    it('should give all the details of the questions ', async function () {
+      const message = 'SQLITE_ERROR: no such table: questions';
+      const actual = await dataStore.getAllQuestions()
+        .catch(err => assert.deepStrictEqual(err.message, message));
+      assert.isUndefined(actual);
+    });
+  });
+
+  context('findUser', () => {
+    it('should name something', async () => {
+      const message = 'SQLITE_ERROR: no such table: users';
+      const actual = await dataStore.findUser('123')
+        .catch(err => assert.deepStrictEqual(err.message, message));
+      assert.isUndefined(actual);
     });
   });
 });
