f31bd2e0d893c3b70734802cf618322dc50026a1
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-07-30 14:15:31 +0530

    |#14|rivu/ragini|added comment box in answer
    moved toggleComment box function in questionPage.js

--- public/js/questionPage.js
+++ public/js/questionPage.js
@@ -22,3 +22,12 @@ const updateVote = (url, container) => {
     }
   });
 };
+
+const toggleCommentBox = (className) => {
+  const commentBox = document.querySelector(className);
+  if(commentBox.classList.contains('hide')) {
+    commentBox.classList.remove('hide');
+    return '0';
+  }
+  commentBox.classList.add('hide');
+};
--- public/js/showCommentBox.js
+++
@@ -1,8 +0,0 @@
-const showCommentBox = (className) => {
-  const commentBox = document.querySelector(className);
-  if(commentBox.classList.contains('hide')) {
-    commentBox.classList.remove('hide');
-    return '0';
-  }
-  commentBox.classList.add('hide');
-};
