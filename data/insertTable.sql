74c2c9136fb7f567747051093f8973463a1c03f7
rivu <58026024+unphydra@users.noreply.github.com>
2020-08-03 14:47:39 +0530

    |#27|Ragini/Rivu| modified insertTable.sql and insertData.sql

--- data/insertTable.sql
+++ data/insertTable.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users
 (
-  id VARCHAR(30) PRIMARY KEY NOT NULL,
+  id INTEGER PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
   username VARCHAR(30) NOT NULL,
   company VARCHAR(30),
@@ -13,14 +13,12 @@ CREATE TABLE IF NOT EXISTS users
 DROP TABLE IF EXISTS questions;
 CREATE TABLE IF NOT EXISTS questions
 (
-  id VARCHAR(30) PRIMARY KEY NOT NULL,
-  ownerId VARCHAR(30) NOT NULL,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  ownerId INTEGER NOT NULL,
   title VARCHAR(30) NOT NULL,
   body VARCHAR(1000) NOT NULL,
-  votes NUMERIC(5) DEFAULT 0,
   receivedAt Date DEFAULT (datetime('now','localtime')),
   modifiedAt Date DEFAULT (datetime('now','localtime')),
-  anyAnswerAccepted BOOLEAN DEFAULT FALSE,
   FOREIGN KEY (ownerId)
   REFERENCES users(id)
 );
@@ -28,14 +26,13 @@ CREATE TABLE IF NOT EXISTS questions
 DROP TABLE IF EXISTS answers;
 CREATE TABLE IF NOT EXISTS answers
 (
-  id VARCHAR(30) PRIMARY KEY NOT NULL,
-  questionId VARCHAR(30) NOT NULL,
-  ownerId VARCHAR(30) NOT NULL,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  questionId INTEGER NOT NULL,
+  ownerId INTEGER NOT NULL,
   answer VARCHAR(1000) NOT NULL,
   receivedAt Date DEFAULT (datetime('now','localtime')),
   modifiedAt Date DEFAULT (datetime('now','localtime')),
   isAccepted BOOLEAN DEFAULT FALSE,
-  votes NUMERIC(5) DEFAULT 0,
   FOREIGN KEY (ownerId)
   REFERENCES users(id),
   FOREIGN KEY (questionId)
@@ -45,34 +42,44 @@ CREATE TABLE IF NOT EXISTS answers
 DROP TABLE IF EXISTS comments;
 CREATE TABLE IF NOT EXISTS comments
 (
-  id VARCHAR(15) PRIMARY KEY NOT NULL,
-  responseId VARCHAR(30) NOT NULL,
-  ownerId VARCHAR(30) NOT NULL,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  responseId INTEGER NOT NULL,
+  ownerId INTEGER NOT NULL,
   comment VARCHAR(400) NOT NULL,
-  receivedAt DATE NOT NULL DEFAULT (datetime('now','localtime'))
+  type INTEGER NOT NULL,
+  receivedAt DATE DEFAULT (datetime('now','localtime'))
+  CHECK (type=1 OR type=0),
+  FOREIGN KEY (ownerId)
+  REFERENCES users(id)
 );
 
 DROP TABLE IF EXISTS voteLog;
 CREATE TABLE IF NOT EXISTS voteLog
 (
-  ownerId VARCHAR(30),
-  responseId VARCHAR(30),
-  vote BOOLEAN,
-  PRIMARY KEY (ownerId,responseId)
+  ownerId INTEGER NOT NULL,
+  responseId INTEGER NOT NULL,
+  vote INTEGER NOT NULL,
+  type INTEGER NOT NULL,
+  PRIMARY KEY (ownerId,responseId,type),
+  CHECK (type=1 OR type=0),
+  CHECK (vote=1 OR vote=-1),
+  FOREIGN KEY (ownerId)
+  REFERENCES users(id)
 );
 
 DROP TABLE IF EXISTS tags;
 CREATE TABLE IF NOT EXISTS tags
 (
-  id VARCHAR(30) UNIQUE,
+  id INTEGER PRIMARY KEY AUTOINCREMENT ,
   title VARCHAR(30)
 );
 
 DROP TABLE IF EXISTS questionTags;
 CREATE TABLE IF NOT EXISTS questionTags
 (
-  questionId VARCHAR(10),
-  tagId VARCHAR(30),
+  questionId INTEGER NOT NULL,
+  tagId INTEGER NOT NULL,
+  PRIMARY KEY (questionId,tagId),
   FOREIGN KEY (questionId)
   REFERENCES questions(id),
   FOREIGN KEY (tagId)
